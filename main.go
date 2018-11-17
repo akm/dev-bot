7e43eaf4ea9a814bf1957c436ecf4ea73dc8107e
akm <akm2000@gmail.com>
2018-11-17 14:25:51 +0900

    :+1: Implement subscribeSlack for Slack Events API
    https://api.slack.com/events-api#subscriptions

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,10 +14,12 @@ import (
 
 	"github.com/google/go-github/github"
 	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 )
 
 func main() {
 	http.HandleFunc("/hello", sayHello)
+	http.HandleFunc("/slack/subscribe", subscribeSlack)
 	http.HandleFunc("/github/pull_requests", showPullRequestSummary)
 
 	appengine.Main()
@@ -24,6 +28,76 @@ func main() {
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello!")
 }
+
+func subscribeSlack(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	slack_api := slack.New(os.Getenv("SLACK_OAUTH_ACCESS_TOKEN"))
+	slack.OptionHTTPClient(urlfetch.Client(ctx))(slack_api)
+
+	// https://api.slack.com/events-api#subscriptions
+	// https://github.com/nlopes/slack
+	// https://github.com/nlopes/slack/blob/master/examples/eventsapi/events.go
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	body := buf.String()
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{os.Getenv("SLACK_VERIFICATION_TOKEN")}))
+	if e != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	log.Debugf(ctx, "eventsAPIEvent: %v\n", eventsAPIEvent)
+	log.Debugf(ctx, "eventsAPIEvent.Type: %v\n", eventsAPIEvent.Type)
+
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "text")
+		w.Write([]byte(r.Challenge))
+	}
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+
+		log.Debugf(ctx, "innerEvent: [%T] %v\n", innerEvent, innerEvent)
+		log.Debugf(ctx, "innerEvent.Data: [%T] %v\n", innerEvent.Data, innerEvent.Data)
+
+		botInfo, err := slack_api.GetBotInfo("")
+		if err != nil {
+			log.Errorf(ctx, "Failed to slack_api.GetBotInfo because of %v\n", err)
+			return
+		}
+
+		var msg string
+		var channel string
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			if botInfo.ID == ev.User {
+				return
+			}
+			channel = ev.Channel
+			msg = fmt.Sprintf("<@%s> said to me %s", ev.User, ev.Text)
+		case *slackevents.MessageEvent:
+			if botInfo.ID == ev.User {
+				return
+			}
+			channel = ev.Channel
+			msg = fmt.Sprintf("<@%s> said %s", ev.User, ev.Text)
+		default:
+			return
+		}
+		postParams := slack.PostMessageParameters{}
+		channelID, timestamp, err := slack_api.PostMessage(channel, msg, postParams)
+		if err != nil {
+			log.Errorf(ctx, "Failed to slack_api.PostMessage because of %v\n", err)
+			return
+		}
+		log.Debugf(ctx, "Succeed to slack_api.PostMessage channedID: %v, timestap: %v\n", channelID, timestamp)
+	}
+}
+
 
 func showPullRequestSummary(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
